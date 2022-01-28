3b9b000f0d6ce5b3cf91d2731ff92ed02762e311
Elizarov Vladimir <elizarov@moedelo.org>
2022-01-28 21:57:58 +0300

    problem-1

---
+++ src/project_euler/problem1.clj
@@ -0,0 +1,31 @@
+(ns project-euler.problem1)
+
+(defn filterMod 
+  [divider x]
+  (= 0 (mod x divider)))
+
+(def mod3 (partial filterMod 3))
+(def mod5 (partial filterMod 5))
+
+(defn get-value-or-null
+  [x]
+  (cond
+    (mod3 x) x
+    (mod5 x) x
+    :else 0))
+
+;(defn acc 
+;  [x y]
+;  (+ (get-value-or-null x) (get-value-or-null y)))
+
+(defn get-sum 
+  [items]
+  (reduce
+   (fn [acc number] (+ (get-value-or-null number) acc))
+   0
+   items)
+  )
+
+(defn -main
+  []
+  (println (get-sum (range (inc 1000)))))
